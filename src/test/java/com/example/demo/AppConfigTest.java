d12d09bc8f3f8eb2f4a540365cbbcbc29412aa71
Geertjan Wielenga <geertjan@apache.org>
2026-08-02 14:12:40 +0200

    test: add context load test covering RestTemplate timeout config

---
+++ src/test/java/com/example/demo/AppConfigTest.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.client.RestTemplate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class AppConfigTest {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Test
+    void restTemplateBeanIsConfigured() {
+        assertThat(restTemplate).isNotNull();
+    }
+}
